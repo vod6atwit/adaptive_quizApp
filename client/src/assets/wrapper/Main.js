fa55c98c211071b98d460aa15d79abf7fafaf9db
Duyvo1117 <duyvo1117@gmail.com>
2023-02-07 11:16:44 -0500

    completed main page frontend

--- client/src/assets/wrapper/Main.js
+++ client/src/assets/wrapper/Main.js
@@ -1,5 +1,37 @@
 import styled from 'styled-components';
 
-const Wrapper = styled.div``;
+const Wrapper = styled.section`
+  /* background-color: red; */
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: var(--primary-50);
+    border-radius: var(--borderRadius);
+    border: 0.25rem solid var(--primary-500);
+    padding: 2rem 2rem;
+    width: var(--fluid-width);
+  }
+  .title {
+    text-transform: uppercase;
+    font-size: 2.75rem;
+  }
+  ol {
+    margin-bottom: 2rem;
+  }
+  #form {
+    margin-bottom: 1rem;
+  }
+  li {
+    font-size: 1.25rem;
+  }
+  .form-input {
+    background-color: var(--white);
+  }
+`;
 
 export default Wrapper;
--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,36 +1,42 @@
 import React from 'react';
 import { useRef } from 'react';
 import { Link } from 'react-router-dom';
+import Wrapper from '../assets/wrapper/Main';
 
 const Main = () => {
   const inputRef = useRef(null);
 
   return (
-    <div className="container">
-      <h1 className="title text-light">Read before you start the quiz</h1>
+    <Wrapper>
+      <div className="container">
+        <h1 className="title text-light">Read before you start the quiz</h1>
 
-      <ol>
-        <li>
-          <h5>You will be asked 10 questions one after another.</h5>
-        </li>
-        <li>10 points is awared for the correct answer.</li>
-        <li>
-          Each question has three options. You can choose only one options.
-        </li>
-        <li>You can review and change answers before the quiz finish.</li>
-        <li>The result will be declared at the end of the quiz.</li>
-      </ol>
+        <ol>
+          <li>You will be asked 10 questions one after another.</li>
+          <li>10 points is awared for the correct answer.</li>
+          <li>
+            Each question has three options. You can choose only one options.
+          </li>
+          <li>You can review and change answers before the quiz finish.</li>
+          <li>The result will be declared at the end of the quiz.</li>
+        </ol>
 
-      <form id="form">
-        <input ref={inputRef} type="text" placeholder="Username*" />
-      </form>
+        <form id="form">
+          <input
+            ref={inputRef}
+            type="text"
+            placeholder="Username*"
+            className="form-input"
+          />
+        </form>
 
-      <div>
-        <Link className="btn" to="/quiz">
-          Start Quiz
-        </Link>
+        <div>
+          <Link className="btn" to="/quiz">
+            Start Quiz
+          </Link>
+        </div>
       </div>
-    </div>
+    </Wrapper>
   );
 };
 
